12dd191d6318703c28c349c1306933ee2a4e9069
warp-core <warp-core@users.noreply.github.com>
2021-10-31 19:06:49 +0000

    CRConvert and Template now support slash commands

--- src/main/java/me/mcofficer/james/commands/creatortools/Template.java
+++ src/main/java/me/mcofficer/james/commands/creatortools/Template.java
@@ -1,14 +1,22 @@
 package me.mcofficer.james.commands.creatortools;
 
-import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import me.mcofficer.james.James;
+import me.mcofficer.james.JamesSlashCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
-public class Template extends Command {
+public class Template extends JamesSlashCommand {
 
+    private static final String optionName = "key";
     private HashMap<String, String> templates = new HashMap<>() {
         {
             put("plugin", "exampleplugin.zip");
@@ -19,10 +27,36 @@ public class Template extends Command {
 
     public Template() {
         name = "template";
-        help = "Serves a template X for content creators. Available templates are: "
-                + String.join(", ", templates.keySet());
+        //help = "Serves a template X for content creators. Available templates are: "
+        //        + String.join(", ", templates.keySet());
+        help = "Serves the chosen template.";
         category = James.creatorTools;
         arguments = "X";
+
+        List<OptionData> data = new ArrayList<>();
+        data.add(new OptionData(OptionType.STRING, optionName, "The key for the template to get.", true).addChoices(generateChoices()));
+        this.options = data;
+    }
+
+    private List<Command.Choice> generateChoices() {
+        List<Command.Choice> choices = new ArrayList<>();
+        String[] keys = templates.keySet().toArray(new String[0]);
+        for (String key : keys) {
+            choices.add(new Command.Choice(key, key));
+        }
+        return choices;
+    }
+
+    @Override
+    protected void execute(SlashCommandEvent event) {
+        String key = event.getOption(optionName).getAsString();
+        String url = James.GITHUB_RAW_URL + "data/templates/" + templates.get(key);
+        event.replyEmbeds(new EmbedBuilder()
+            .setTitle(templates.get(key), url)
+            .setColor(event.getGuild().getSelfMember().getColor())
+            .setDescription("Here's your template, served hot and crunchy :)")
+            .build()
+        ).queue();
     }
 
     @Override
